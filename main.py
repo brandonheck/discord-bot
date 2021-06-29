b8716c1dde661206f8bbacf78f2e690355139209
Kenny Ingle <kentron@gmail.com>
2021-06-29 15:32:24 +0000

    adds trivia and meme commands.  fix formatting

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 import urllib
+import random
+import html
 from xml.dom.minidom import parseString
 from discord.ext import commands
 
@@ -10,53 +12,100 @@ yelp_token = os.environ['YELP_TOKEN']
 pbotid = os.environ['PBOTID']
 pcustid = os.environ['PCUSTID']
 
+
 @bot.command(name='cat', help='Responds with a random cat picture')
 async def cat(ctx):
-  response = requests.get("https://api.thecatapi.com/v1/images/search")
-  json_data = json.loads(response.text)
-  catURL = json_data[0]["url"]
-  await ctx.send(catURL)
+    response = requests.get("https://api.thecatapi.com/v1/images/search")
+    json_data = json.loads(response.text)
+    catURL = json_data[0]["url"]
+    await ctx.send(catURL)
 
-@bot.command(name='pitch', help='Responds with a random sales pitch business idea')
+
+@bot.command(name='pitch',
+             help='Responds with a random sales pitch business idea')
 async def get_pitch(ctx):
-  response = requests.get("https://itsthisforthat.com/api.php?text")
-  await ctx.send(response.text)
+    response = requests.get("https://itsthisforthat.com/api.php?text")
+    await ctx.send(response.text)
+
 
 @bot.command(name='zen', help='Responds with a random zen quote')
 async def get_quote(ctx):
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
-  await ctx.send(quote)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+    await ctx.send(quote)
+
 
 @bot.command(name='mom', help='Responds with a random Yo Momma! joke')
 async def get_momjoke(ctx):
-  response = requests.get("https://api.yomomma.info/")
-  json_data = json.loads(response.text)
-  joke = json_data["joke"]
-  await ctx.send(joke)
+    response = requests.get("https://api.yomomma.info/")
+    json_data = json.loads(response.text)
+    joke = json_data["joke"]
+    await ctx.send(joke)
+
 
 @bot.command(name='dad', help='Responds with a random dad joke')
 async def get_dadjoke(ctx):
-  response = requests.get("https://icanhazdadjoke.com/slack")
-  json_data = json.loads(response.text)
-  joke = json_data["attachments"][0]["fallback"]
-  await ctx.send(joke)
+    response = requests.get("https://icanhazdadjoke.com/slack")
+    json_data = json.loads(response.text)
+    joke = json_data["attachments"][0]["fallback"]
+    await ctx.send(joke)
+
 
 @bot.command(name='ama', help='Ask me anything')
 async def get_test(ctx, *, args):
-  response = requests.get("https://www.pandorabots.com/pandora/talk-xml?botid=" + pbotid + "&custid=" + pcustid + "&input=" + urllib.parse.quote(args))
-  dom3 = parseString(response.text)
-  name = dom3.getElementsByTagName('that')
-  await ctx.send(name[0].firstChild.nodeValue)
+    response = requests.get(
+        "https://www.pandorabots.com/pandora/talk-xml?botid=" + pbotid +
+        "&custid=" + pcustid + "&input=" + urllib.parse.quote(args))
+    dom3 = parseString(response.text)
+    name = dom3.getElementsByTagName('that')
+    await ctx.send(name[0].firstChild.nodeValue)
 
-@bot.command(name='yelp', help='Search yelp for businesses matching your search')
+
+@bot.command(name='yelp',
+             help='Search yelp for businesses matching your search')
 async def get_yelp(ctx, *, args):
-  response = requests.get("https://api.yelp.com/v3/businesses/search?location=64081&limit=10&term=" + urllib.parse.quote(args), headers={"Authorization": "Bearer " + yelp_token})
-  json_data = json.loads(response.text)
-  results = "Here are some suggestions:"
-  for business in json_data["businesses"]:
-    results += '\n' + business['name'] + " " + str(business['rating']) + " stars "
-  await ctx.send(results)
+    response = requests.get(
+        "https://api.yelp.com/v3/businesses/search?location=64081&limit=10&term="
+        + urllib.parse.quote(args),
+        headers={"Authorization": "Bearer " + yelp_token})
+    json_data = json.loads(response.text)
+    results = "Here are some suggestions:"
+    for business in json_data["businesses"]:
+        results += '\n' + business['name'] + " " + str(
+            business['rating']) + " stars "
+    await ctx.send(results)
+
+
+@bot.command(name='meme', help="Get you a random meme from reddit")
+async def get_meme(ctx):
+    response = requests.get("https://meme-api.herokuapp.com/gimme/dankmemes")
+    json_data = json.loads(response.text)
+    memeURL = json_data["url"]
+    await ctx.send(memeURL)
+
+
+@bot.command(name='trivia', help="Displays a random trivia fact")
+async def get_trivia(ctx):
+    response = requests.get("https://opentdb.com/api.php?amount=1")
+    json_data = json.loads(response.text)
+    question = json_data["results"][0]["question"]
+    correctAnswer = json_data["results"][0]["correct_answer"]
+    incorrectAnswers = json_data["results"][0]["incorrect_answers"]
+    incorrectSize = len(incorrectAnswers)
+    allAnswers = [correctAnswer, incorrectAnswers[0]]
+    if incorrectSize > 1:
+        allAnswers.append(incorrectAnswers[1])
+        allAnswers.append(incorrectAnswers[2])
+    random.shuffle(allAnswers)
+    result = question + '\n' + "A: " + allAnswers[
+        0] + '\n' + "B: " + allAnswers[1]
+    if incorrectSize > 1:
+        result += '\n' + "C: " + allAnswers[2] + '\n' + "D: " + allAnswers[3]
+    result += '\n' + "Correct Answer : " + "||" + correctAnswer.ljust(
+        50, ' ') + "||"
+    unescapedResult = html.unescape(result)
+    await ctx.send(unescapedResult)
+
 
 bot.run(os.environ['TOKEN'])
